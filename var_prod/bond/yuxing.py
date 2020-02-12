88a5b1abd2b6a10254691a69c2f8c2525c96eb97
acanacar <ahmetcanacar.1@gmail.com>
2020-02-12 12:47:06 +0300

    soruces are added before a1

--- var_prod/bond/yuxing.py
+++ var_prod/bond/yuxing.py
@@ -1,8 +1,20 @@
 from matplotlib.pyplot import *
-time=[3/12,6/12,2,3,5,10,30]
-rate=[0.47,0.6,1.18,1.53,2,2.53,3.12]
+
+time = [3 / 12, 6 / 12, 2, 3, 5, 10, 30]
+rate = [0.47, 0.6, 1.18, 1.53, 2, 2.53, 3.12]
 title("Term Structure of Interest Rate ")
 xlabel("Time ")
 ylabel("Risk-free rate (%)")
-plot(time,rate)
+plot(time, rate)
 show()
+
+import pandas as pd
+import numpy as np
+
+x = pd.Series([1, 2, np.nan, np.nan, 6])
+x.interpolate()
+
+
+nan = np.nan
+x=pd.Series([2,nan,nan,nan,nan,2.53])
+x.interpolate()
